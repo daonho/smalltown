38b4724d883e6d5bd2fb54b59ce6e5bad9eea111
hung <hoanghung.cuamung@gmail.com>
2021-06-12 14:01:08 +0700

    the first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const request = require("request")
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res){
+  if(req.body.tk === "hungducbl" & req.body.mk === "Yenxinhgaiso1"){
+    res.sendFile(__dirname  + "/success.html");
+  }
+  else {
+    res.sendFile(__dirname  + "/failure.html");
+  }
+
+});
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("server is runing on port 3000")
+})
